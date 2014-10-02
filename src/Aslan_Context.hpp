fc64a44082b2e1d41c418d5e8f8a071b087feaf7
Ricardo Lucca <rlucca@gmail.com>
2014-10-01 21:47:37 -0300

    Putting some doxygen commentary
    I do not intent to generate doxygen... but it is a good thing.

--- src/Aslan_Context.hpp
+++ src/Aslan_Context.hpp
@@ -4,11 +4,14 @@
 
 using namespace std;
 
+/** It's the proxy between the program and the flex/yacc.
+	It keep the scanner and a reference to where get
+	new data (cin or a istringstream). */
 class Aslan_Context
 {
 public:
-		void *scanner;	// the scanner state
-		istream* is;	// input stream
+		void *scanner;	//< the scanner state
+		istream* is;	//< input stream
 
 		Aslan_Context(istream* input = &cin);
 		virtual ~Aslan_Context();
